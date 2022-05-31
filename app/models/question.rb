dc16defdfb7646475a0cc78a733d8a7e9eba43e0
Madhusudan Tripathy <madhusudantripathy77@gmail.com>
2022-05-31 18:16:15 +0530

    added tag validation to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ApplicationRecord
     has_many :answers, dependent: :destroy
     has_many :question_reactions, dependent: :destroy
     validates :question, presence: true
+    validates :tag, presence: true
 end
