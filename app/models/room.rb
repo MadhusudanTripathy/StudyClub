683ac5a63e9a3018eb151e2ca740b7c8ba405ed5
Madhusudan Tripathy <madhusudantripathy77@gmail.com>
2022-05-31 22:27:31 +0530

    css fixed

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   
   validates_uniqueness_of :name
-  has_many :messages
+  has_many :messages, dependent: :destroy
   after_create_commit {broadcast_append_to "rooms"}
 
 end
